a4461430ee3e1eb1079f36a5f2ccc7f0961ef62d
Andy Han <dhan@macalester.edu>
2017-04-24 22:29:05 -0500

    Push #2

--- MaxCalesterMainProgram.py
+++ MaxCalesterMainProgram.py
@@ -1,6 +1,6 @@
 #   MaxCalester Main Program
 #   Developed By: Andy Han
-#   Data Provided By: Ibou Dieye and Lang Li
+#   Data Provided By: Ibrahima Dieye and Lang Li
 #   March 29th, 2017
 #   Macalester College
 #   COMP 221 - Algorithm Design and Analysis
@@ -16,8 +16,10 @@ from MaxCalesterProgramTacoBell import*
 
 def restaurant_picker():
     """"This is the function that is responsible for jump starting the program. """
-    print("Welcome to MaxCalester - a program designed to give you a selection of items to buy at fast food restaurants")
-    print("Given a budget, we make our choice by giving you a list of items that would yield the highest calorie count.")
+    print("Welcome to MaxCalester - a program that solves your fast-food restaurants dilemma!")
+    print("Given a budget, the program yields a list of items that would yield the highest calorie count.")
+    print("Our rationale is the most calorie you get, more value you get for your $$$!")
+    print("You get to choose the restaurant or we can pick for you! Or, we can give you a combo of all restaurants!")
     print("The restaurants in our database are: Subway, Chipotle, Domino's, McDonalds, and Taco Bell. Let's go!")
     print("----------------------------------------------------------------------------")
     budget = int(input("What is your budget? "))
@@ -26,34 +28,46 @@ def restaurant_picker():
                            "Type YES for a cross-selection and NO for a single-selection. "))
     print("----------------------------------------------------------------------------")
 
-    if user_input == "YES":
-        start_time = time.time()
-        all_restaurants_main_driver(budget)
-        all_restaurant_max_cal = all_restaurants_best_combo_price_calorie[budget][1]
-        all_restaurant_max_price = all_restaurants_best_combo_price_calorie[budget][0]
+    if user_input == "YES":  # If the user wants a cross-selection of items.
+        start_time = time.time()  # The program also measures the execution time of the program
+
+        all_restaurants_main_driver(budget)  # Calls the main driver of the Python file for this option
+        all_restaurant_max_cal = all_restaurants_best_combo_price_calorie[budget][1]  # The total calorie count
+        all_restaurant_max_price = all_restaurants_best_combo_price_calorie[budget][0] # The total price
 
         print("At your budget of $" + str(budget) +
               ", to get the most bang for your buck in terms of highest net calorie count, ")
         print("You should purchase the following items across all five restaurants to obtain a total caloric count of ")
         print(str(all_restaurant_max_cal) + " calories" + " for the price of $" + str(all_restaurant_max_price))
         print("----------------------------------------------------------------------------")
-        print(*all_restaurants_item_list, sep="\n")
+        print(*all_restaurants_item_list, sep="\n")  # Prints all of the items in the recommendation
         print("Thank you for using our program and happy eating!")
         print("----------------------------------------------------------------------------")
-        print("It took %s seconds to get the optimal." % (time.time() - start_time))
+        print("It took %s seconds to calculate this recommendation." % (time.time() - start_time))
 
-    elif user_input == "NO":
+    elif user_input == "NO":  # If the user does not want a cross-selection of restaurants.
         print("You chose NO. Would you like to choose the single restaurant you would like a recommendation from or"
               "the restaurant with the best recommendation in terms of calorie count?")
         restaurant_choice = input(str("Please type BEST for the best pick or the name of the restaurant "
                                       "you would like to choose from. Please, all lower case and no spaces. "))
         if restaurant_choice == "BEST" or "subway" or "tacobell" or "mcdonalds" or "chipotle" or "dominos":
-            start_time = time.time()
+            # spelling and case really matter. No capitalization. No spaces.
+
+            start_time = time.time()  # We don't want to count the time the user spends typing.
+
+            # What's happening below is that we run the budget for every restaurant and
+            # See which restaurant has the best combo that would yield the highest calorie count
+
             subway_main_driver(budget)
             tacobell_main_driver(budget)
             mcdonalds_main_driver(budget)
             chipotle_main_driver(budget)
             dominos_main_driver(budget)
+
+            # Below are the variables that will hold the numbers for total calorie and price
+            # for each each respective restaurants. Good use of double arrays.
+            # Most of the heavy lifting is done by the other files, we even carry over the variables.
+            # Do note again that adding new restaurants means making new files for them.
 
             subway_max_cal = subway_best_combo_price_calorie[budget][1]
             tacobell_max_cal = tacobell_best_combo_price_calorie[budget][1]
@@ -73,6 +87,11 @@ def restaurant_picker():
                                           dominos_max_cal: "dominos"})
 
             max_cal = max(subway_max_cal, tacobell_max_cal, mcdonalds_max_cal, chipotle_max_cal, dominos_max_cal)
+
+            # Obviously, the max_cal will be equal to one of the five restaurants' numbers or
+            # the user will specifically ask for Subway out of the five restaurants.
+            # We cannot emphasize how case sensitive the program is.
+            # The conditionals below reflect what the program will say if our winner is a certain restaurant.
 
             if restaurant_dictionary.get(max_cal) == "subway" or restaurant_choice == "subway":
                 print("At your budget of $" + str(budget) + ", to get the highest net calorie count, ")
@@ -117,10 +136,10 @@ def restaurant_picker():
                 print("Thank you for using our program and happy eating!")
 
             print("----------------------------------------------------------------------------")
-            print("It took %s seconds to get the optimal." % (time.time() - start_time))
+            print("It took %s seconds to calculate this recommendation." % (time.time() - start_time))
 
         else:
-            print("You typed an invalid command. Try again!")
+            print("You typed an invalid command. Try again!")  # We are well aware that users make mistakes.
             restaurant_picker()
 
 restaurant_picker()
--- MaxCalesterProgramAllRestaurants.py
+++ MaxCalesterProgramAllRestaurants.py
@@ -1,9 +1,11 @@
 #   MaxCalester Program All Restaurants
 #   Developed By: Andy Han
-#   Data Provided By: Ibou Dieye and Lang Li
+#   Data Provided By: Ibrahima Dieye and Lang Li
 #   March 29th, 2017
 #   Macalester College
 #   COMP 221 - Algorithm Design and Analysis
+
+# This file has the most extensive comments out of all of the Python files for each restaurant. Use this as a reference.
 
 import csv
 
@@ -19,6 +21,9 @@ all_restaurants_calorie = []  # List of the calories of said items.
 all_restaurants_ratio = []  # List of price-to-calorie ratios of said items.
 
 # Below are the lines of code that are responsible for the import of csv data into data points Python can work with.
+# A great thing about our data set is that they are already well-organized and sorted by the calorie-price ratio.
+# However, we do have sorting in place in case the data isn't perfect as in a real life, data will always be added.
+
 with open('final_all_items.csv', 'r') as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
@@ -48,7 +53,7 @@ all_restaurants_item_list = []  # The list of items you should buy to maximize c
 
 
 def all_restaurants_main_driver(budget):
-    if all_restaurants_best_combo.get(budget):
+    if all_restaurants_best_combo.get(budget):  # Checks to see if a value already exists in the dictionary. Saves time.
         return print("For the budget of $" + str(budget) +
                      ", here is the list of items you should buy to maximize how much calorie you are getting: "
                      + str(all_restaurants_best_combo.get(budget)))
@@ -66,10 +71,12 @@ def greedy_algorithm(budget):
         return
 
     if calorie_price_dict.get(sorted_calorie_list[0]) <= budget <= ratio_price_dict.get(sorted_ratio_list[1]) * 2:
-        all_restaurants_item_list.append("1 " + calorie_name_dict.get(sorted_calorie_list[0]) + " for the price of $"
-                         + str(calorie_price_dict.get(sorted_calorie_list[0])) +
-                         " with a calorie count of " + str(sorted_calorie_list[0]))
 
+        all_restaurants_item_list.append("1 " + calorie_name_dict.get(sorted_calorie_list[0]) + " for the price of $"
+                                         + str(calorie_price_dict.get(sorted_calorie_list[0])) +
+                                         " with a calorie count of " + str(sorted_calorie_list[0]))
+
+        # Add the results to our "database" dictionary
         all_restaurants_best_combo.update({budget: all_restaurants_item_list})
         all_restaurants_best_combo_price_calorie[budget] = [total_price, total_calorie_count]
 
@@ -80,15 +87,17 @@ def greedy_algorithm(budget):
         item_name = ratio_name_dict.get(current_ratio)
         item_price = ratio_price_dict.get(current_ratio)
 
-        if total_price + item_price <= budget:  # If what you already spent plus the price of the item you want to purchase is less than the budget...
+        # If what you already spent plus the price of the item you want to purchase is less than the budget.
+        if total_price + item_price <= budget:
             total_price = total_price + item_price  # Add the desired item's price on to the total price tally
-            total_calorie_count = total_calorie_count + name_calorie_dict.get(
-                item_name)  # Add the desired item's caloric count to the total caloric tally
+            total_calorie_count = total_calorie_count + name_calorie_dict.get(item_name)
+            # Add the desired item's caloric count to the total caloric tally
             all_restaurants_item_list.append("1 " + item_name + " for the price of $" + str(item_price) +
                                              " with a calorie count of " + str(
                 name_calorie_dict.get(item_name)))  # Add the item to the final list
 
-    all_restaurants_best_combo[budget] = all_restaurants_item_list  # Add the list of items that is best at the price point to dictionary so that future searches are O(1)
+    # We are adding the list of items that is best at the price point to the dict so that future searches are O(1)
+    all_restaurants_best_combo[budget] = all_restaurants_item_list
     all_restaurants_best_combo_price_calorie[budget] = [total_price, total_calorie_count]
 
 
